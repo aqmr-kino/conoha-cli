cf4d701cedc3e6820cc2a2d064203c6b077a1ae6
aqmr-kino <32405491+aqmr-kino@users.noreply.github.com>
2020-05-05 18:35:20 +0900

    Add ConoHa billing management module

--- conoha/account/billing.go
+++ conoha/account/billing.go
@@ -36,6 +36,26 @@ type BillingInvoices struct {
 	Invoices []BillingInvoice `json:"billing_invoices"`
 }
 
+// PaymentHistory :
+// 入金履歴
+type PaymentHistory struct {
+	MoneyType     string `json:"money_type"`
+	DepositAmount int    `json:"deposit_amount"`
+	ReceivedDate  string `json:"received_date"`
+}
+
+// PaymentHistories :
+// 入金履歴一覧
+type PaymentHistories struct {
+	Histories []PaymentHistory `json:"payment_history"`
+}
+
+// PaymentSummary :
+// 入金状況
+type PaymentSummary struct {
+	TotalDepositAmount int `json:"total_deposit_amount"`
+}
+
 // BillingManager :
 // 入出金管理
 //   Token:    Conoha API アクセストークン
@@ -99,6 +119,66 @@ func (mgr *BillingManager) GetInvoices() (*BillingInvoices, error) {
 	if err2 != nil {
 		return nil, err2
 	}
+
+	return &ret, nil
+}
+
+//
+// 入金履歴操作
+//
+
+// GetPaymentHistories :
+// 入金履歴一覧の取得
+func (mgr *BillingManager) GetPaymentHistories() (*PaymentHistories, error) {
+	var ret PaymentHistories
+
+	uri := mgr.Endpoint + "/payment-history"
+	byteArray, _, err := mgr.Token.Request(&api.APIRequest{
+		URI:       uri,
+		Method:    api.GET,
+		GetParams: nil,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	err2 := json.Unmarshal(byteArray, &ret)
+
+	if err2 != nil {
+		return nil, err2
+	}
+
+	return &ret, nil
+}
+
+// GetPaymentSummary :
+// 入金状況取得
+func (mgr *BillingManager) GetPaymentSummary() (*PaymentSummary, error) {
+	var ret PaymentSummary
+
+	uri := mgr.Endpoint + "/payment-summary"
+	byteArray, _, err := mgr.Token.Request(&api.APIRequest{
+		URI:       uri,
+		Method:    api.GET,
+		GetParams: nil,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var tmp struct {
+		Summary PaymentSummary `json:"payment_summary"`
+	}
+
+	err2 := json.Unmarshal(byteArray, &ret)
+
+	if err2 != nil {
+		return nil, err2
+	}
+
+	ret = tmp.Summary
 
 	return &ret, nil
 }
